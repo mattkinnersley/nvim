5f8a9a52508ef2a2086a8798fc9da77972b0178f
Matt Kinnersley <kinnersleymatt@gmail.com>
2024-03-02 21:15:40 +0000

    Sync

---
+++ lua/mattkinnersley/plugins.lua
@@ -0,0 +1,22 @@
+return {
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"nvim-telescope/telescope.nvim", tag = '0.1.5',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+	{
+		"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
+	},
+	{'williamboman/mason.nvim'},
+	{'williamboman/mason-lspconfig.nvim'},
+	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+	{'neovim/nvim-lspconfig'},
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/nvim-cmp'},
+	{'L3MON4D3/LuaSnip'},
+}
---
+++ lua/mattkinnersley/set.lua
@@ -0,0 +1,41 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.swapfile = false
+
+vim.opt.scrolloff = 5
+
+vim.opt.updatetime = 1000
+
+vim.g.mapleader = " "
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
